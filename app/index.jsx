44576060e1354483a631935967b81a09d1987b17
Ankit <sam.megh0305@gmail.com>
2025-07-05 11:14:39 +0530

    project congigured

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+   <View className="flex-1 items-center justify-center bg-red-100">
+  <Text className="text-xl text-blue-600 font-bold">Tailwind is working! 🎉</Text>
+</View>
+  );
+}
