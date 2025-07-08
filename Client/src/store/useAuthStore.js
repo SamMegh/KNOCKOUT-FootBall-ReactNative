55527bbc96519ea5635b2d42c41d028757b72722
Ankit <sam.megh0305@gmail.com>
2025-07-08 13:48:01 +0530

    axios is configured for req, res from backend

---
+++ Client/src/store/useAuthStore.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const Instance = axios.create({
+        baseURL: import.meta.env.MODE==="development"?'http://localhost:8081/api/':"/api",
+    withCredentials: true,
+})
---
+++ Client/src/utils/axios.configuration.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const Instance = axios.create({
+        baseURL: import.meta.env.MODE==="development"?'http://localhost:8081/api/':"/api",
+    withCredentials: true,
+})
+export default Instance;
