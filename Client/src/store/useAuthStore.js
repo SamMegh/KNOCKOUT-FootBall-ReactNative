1b91778229251aa7b18d9c03c1cb88c16f161fde
Ankit <sam.megh0305@gmail.com>
2025-07-17 18:44:46 +0530

    deleting socket becouse app is already fast

--- Client/src/store/useAuthStore.js
+++ Client/src/store/useAuthStore.js
@@ -1,18 +1,13 @@
-import { io } from 'socket.io-client';
 import { create } from 'zustand';
 import Instance from '../utils/axios.configuration';
 
-const MAINURL='http://localhost:8080/'
-export const useAuthStore= create((set,get)=>({
+export const useAuthStore= create((set)=>({
     isAuthUser:null,
-    socket:null,
     login:async(data)=>{
         try {
             const res= await Instance.post("/auth/login",data);
             set({isAuthUser:res.data.user});
-            get().connectSocket();
         } catch (error) {
-            get().disconnectSocket();
             console.log(error);
         }
     },
@@ -21,28 +16,12 @@ export const useAuthStore= create((set,get)=>({
         try {
             const res= await Instance.post("/auth/signup",data);
             set({isAuthUser:res.data});
-            get().connectSocket();
         } catch (error) {
-            get().disconnectSocket();
             console.log(error);
         }
     },
     logout: ()=>{
-        get().disconnectSocket();
         set({isAuthUser:null});
     },
 
-    connectSocket:()=>{
-        const {isAuthUser} = get();
-        if (!isAuthUser || get().socket?.connected) return;
-        const socket = io(MAINURL,{
-            query:{
-                userId:isAuthUser._id
-            }
-        });
-        set({socket});
-    },
-    disconnectSocket: () => {
-    if(get().socket?.connected) get().socket.disconnect();
-  },
 }))
