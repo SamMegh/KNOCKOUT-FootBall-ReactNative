bc9a52d45d45e2505f41d68dfa5d8729129a714b
Ankit <sam.megh0305@gmail.com>
2025-07-18 13:45:16 +0530

    frontend working from know

--- Client/app/(nav)/home.jsx
+++ Client/app/(nav)/home.jsx
@@ -1,17 +1,10 @@
 import { Text, View } from 'react-native';
 import { useLeagueStore } from '../../src/store/useLeagueStore';
-import { useEffect } from 'react';
 export default function Home() {
   const {getDayData}= useLeagueStore();
-  useEffect(()=>{
-getDayData();
-  },[getDayData])
   return (
     <View>
       <Text>Welcome to Home</Text>
-      <button onClick={getDayData}>
-        getData
-      </button>
     </View>
   );
 }
