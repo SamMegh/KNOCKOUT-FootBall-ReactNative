89adfe27cd843c8d33eb60be04c8f87690ec9682
Ankit <sam.megh0305@gmail.com>
2025-08-01 18:43:03 +0530

    user databse is change as per profile

--- Backend/src/DBmodel/user.db.model.js
+++ Backend/src/DBmodel/user.db.model.js
@@ -5,20 +5,51 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    firstName: {
+        type: String
+    },
+    LastName: {
+        type: String
+    },
+    userName: {
+        type: String,
+        unque: true
+    },
+    Mobile: {
+        type: String,
+        unque: true
+    },
+    DOB: {
+        type: Date
+    },
     email: {
         type: String,
         required: true,
-    unque: true
+        unque: true
+    },
+    isEmailverified:{
+        type: Boolean,
+        default:false
     },
     password: {
         type: String,
         required: true,
-    minLenght: 6
+        minLenght: 6
     },
+    SCoin: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    GCoin: {
+        type: Number,
+        default: 0,
+        min: 0
+    }
 },
-{
-    timestamps: true
-}
+    {
+        timestamps: true
+    }
 );
 
 const User = mongoose.model('User', userSchema);
--- Client/src/utils/axios.configuration.js
+++ Client/src/utils/axios.configuration.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getItem } from './asyncstorage';
 
 const Instance = axios.create({
-  baseURL: 'https://knockout-football-reactnative.onrender.com',
+  baseURL: 'http://localhost:8080',
   withCredentials: true,
 });
 Instance.interceptors.request.use(async (config) => {
