685ea10994e9f3971f8a1cc11961308631af788e
Ankit <sam.megh0305@gmail.com>
2025-08-03 18:40:49 +0530

    payment frontend logic is done

--- Backend/src/Controller/payment.controller.js
+++ Backend/src/Controller/payment.controller.js
@@ -1,9 +1,45 @@
+import { Cashfree } from "cashfree-pg";
+import crypto from "crypto";
 
 
-export const getsession = async(req, res)=>{
+Cashfree.XClientId = process.env.CASHFREE_CLIENT_ID;
+Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
+Cashfree.XEnvironment = Cashfree.Environment.SANDBOX;
+
+const generateOrderId = async () => {
+    const uniqueId = crypto.randomBytes(16).toString("hex");
+    const hash = crypto.createHash('sha256');
+    hash.update(uniqueId);
+    const orderId = hash.digest('hex');
+    return orderId.slice(0, 12);
+}
+export const getsession = async (req, res) => {
     try {
-        res.status(200).json({message:"server works"})
+        let request = {
+            "order_amount": 1,
+            "order_currency": "INR",
+            "order_id": await generateOrderId(),
+            "customer_details": {
+                "customer_id": "123123123123",
+                "customer_phone": "0000000000",
+                "customer_email": "test0@gmail.com"
+            },
+        };
+
+       const response= await Cashfree.PGCreateOrder("2023-08-01", request);
+
+        res.status(200).json(response.data);
     } catch (error) {
-        res.status(500).json({message:"unable to create session"})
+        res.status(500).json({ message: "unable to create session", error: error.message })
+    }
+}
+
+
+
+export const verify = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "unable to verify the payment", error })
     }
 }
--- Client/src/components/paymentComponent.jsx
+++ Client/src/components/paymentComponent.jsx
@@ -1,38 +1,76 @@
+import { useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
 import Instance from "../utils/axios.configuration";
-
+import { load } from "@cashfreepayments/cashfree-js";
 
 function PaymentComponent() {
+  let cashfree;
+  let insitialzeSDK = async () => {
+    cashfree = await load({
+      mode: "sandbox",
+    });
+  };
+  insitialzeSDK();
+  const { orderId, setOrderId } = useState("");
 
-    
-    
-    // payment part was here 
-    const handlePayment = async ()=>{
-        try {
-            const res = await Instance.get("/payment/initiatesession");
-      if(res){
-        console.log(res)
+  // payment part was here
+  const getSessionId = async () => {
+    try {
+      const res = await Instance.get("/payment/initiatesession");
+      if (res.data && res.data.payment_session_id) {
+        setOrderId(res.data.order_id);
+        return res.data.payment_session_id;
       }
-      console.log("paymentDone");
-        } catch (error) {
-            console.log(error);
-        }
-      
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+
+  const verifyPayment= async()=>{
+    try {
+        const res = Instance.post("/payment/verify",{
+            orderId
+        })
+        if(res && res.data){
+        alert("payment verified")
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  const handlePayment = async () => {
+    try {
+      let sessionId = await getSessionId();
+      let checkoutOptions = {
+        paymentspaymentSessionId: sessionId,
+        redirectTarget: "_modal",
+      };
+      cashfree.checkout(checkoutOptions).then((res) => {
+        console.log("payment initialized")
+
+        verifyPayment(orderId)
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-    <TouchableOpacity onPress={handlePayment} style={{
-                  backgroundColor:"white",
-                  padding:5,
-                  margin:10,
-                  borderRadius:20,
-                }}>
-                  <Text>
-                    Check Payment GetWay
-                  </Text>
-                </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handlePayment}
+        style={{
+          backgroundColor: "white",
+          padding: 5,
+          margin: 10,
+          borderRadius: 20,
+        }}
+      >
+        <Text>Check Payment GetWay</Text>
+      </TouchableOpacity>
     </>
-  )
+  );
 }
 
-export default PaymentComponent
+export default PaymentComponent;
