6573d64b31d9d2d85e3d1862cbde8517709d5572
Ankit <sam.megh0305@gmail.com>
2025-08-12 10:26:10 +0530

    app is working but there is no payment getway is added

--- Client/app/index.jsx
+++ Client/app/index.jsx
@@ -1,36 +1,23 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
 import { useRouter } from "expo-router";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuthStore } from "../src/store/useAuthStore.js";
-import OpenPage from './(otherPages)/openpage.jsx';
+import OpenPage from "./(otherPages)/openpage.jsx";
 
 export default function App() {
-  const [publishableKey, setPublishableKey] = useState('');
-const {isAuthUser,check} = useAuthStore();
+  const { isAuthUser, check } = useAuthStore();
   const router = useRouter();
-  const fetchPublishableKey = async () => {
-    const key = await fetchKey(); // fetch key from your server here
-    setPublishableKey(key);
-  };
 
   useEffect(() => {
-      check();
-  if(isAuthUser){
-    router.replace("/home");
-  }
-    fetchPublishableKey();
-  }, [isAuthUser,fetchPublishableKey,check]);
+    check();
+    if (isAuthUser) {
+      router.replace("/home");
+    }
+  }, [isAuthUser, check]);
 
   return (
-    <StripeProvider
-      publishableKey={publishableKey}
-      merchantIdentifier="merchant.identifier" // required for Apple Pay
-      urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
-    >
     <SafeAreaView className="bg-[#000] flex-1">
-      <OpenPage/>
+      <OpenPage />
     </SafeAreaView>
-    </StripeProvider>
   );
 }
--- Client/src/utils/axios.configuration.js
+++ Client/src/utils/axios.configuration.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getItem } from './asyncstorage';
 
 const Instance = axios.create({
-  baseURL: 'http://192.168.151.138:8080',
+  baseURL: 'http://10.235.181.138:8080',
   withCredentials: true,
 });
 Instance.interceptors.request.use(async (config) => {
